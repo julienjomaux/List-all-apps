4e2067c98e3661698a199416d79abb05ad6f388c
Julien Jomaux <julien.jomaux@gmail.com>
2026-01-25 19:58:39 +0100

    Update App.py

--- App.py
+++ App.py
@@ -2,8 +2,20 @@ import streamlit as st
 
 st.set_page_config(page_title="Streamlit App Launcher", page_icon="🔗")
 
-st.title("📊 Streamlit App Launcher")
+st.title("Streamlit App Launcher")
 st.write("Welcome! Here are quick links to other Streamlit dashboards:")
+st.markdown(
+    """
+Welcome! Here are quick links to all the apps of GEM Energy Analytics
+
+**Data source:** [Regelleistung.net](https://www.regelleistung.net/)
+
+**More insights:** [GEM Energy Analytics](https://gemenergyanalytics.substack.com/)  
+**Connect with me:** [Julien Jomaux](https://www.linkedin.com/in/julien-jomaux/)  
+**Email me:** [julien.jomaux@gmail.com](mailto:julien.jomaux@gmail.com)
+"""
+
+
 
 # You can add as many links as you want in this list
 apps = [
@@ -12,12 +24,12 @@ apps = [
         "url": "https://balancing-be.streamlit.app",
         "desc": "Visualize and explore Belgian grid balancing data, metrics, and insights."
     },
-    # Add more apps below as needed!
-    # {
-    #     "name": "Another Streamlit App",
-    #     "url": "https://another-app-url",
-    #     "desc": "Short description of what this app does."
-    # },
+     Add more apps below as needed!
+     {
+         "name": "FCR capacity prices",
+        "url": "https://fcr-heatmap.streamlit.app/",
+         "desc": "Display FCR capacity prices from 2021 to 2025"
+     },
 ]
 
 for app in apps:
@@ -27,3 +39,4 @@ for app in apps:
     )
     st.write(f"_{app['desc']}_")  # Description in italic
     st.markdown("---")  # Separator line
+
